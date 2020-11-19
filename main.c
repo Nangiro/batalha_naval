da623d34de26df9adc80ebbaee9ff698371a2f07
Fernando Kira <kira.fernando@gmail.com>
2020-11-19 18:00:07 -0300

    Added: Menu Function

--- main.c
+++ main.c
@@ -8,16 +8,36 @@
 
 void welcome()
 {
-    printf(" ---------------\n");
-    printf("| BATALHA NAVAL |\n");
-    printf(" ---------------\n");
+    printf(" -----------------------\n");
+    printf("|\tBATALHA NAVAL\t|\n");
+    printf(" -----------------------\n");
+}
+
+int menu()
+{
+    int selection;
+    printf(" -----------------------\n");
+    printf("|\tMENU\t\t|\n");
+    printf(" -----------------------\n");
+    printf("| 1\tJOGAR\t\t|\n");
+    printf("| 2\tSAIR\t\t|\n");
+    printf(" -----------------------\n");
+    printf("OPÇÃO:\t");
+    scanf("%d", &selection);
+    system("clear"); //LINUX
+    system("cls"); //Windows
+
+    return selection;
 }
 
 int main()
 {
+    int menuSelection;
+
     setlocale(LC_ALL,"portuguese");
 
     welcome();
+    menuSelection = menu();
 
     return 0;
 }
